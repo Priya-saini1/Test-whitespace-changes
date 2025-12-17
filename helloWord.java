ba9d7500950eb4a11469466ea4b06e5f79a96a1a
priya.saini <erpriyasaini2000@gmail.com>
2025-12-17 20:39:56 +0530

    hello-world

---
+++ helloWord.java
@@ -0,0 +1,11 @@
+package com.example.demo;   // optional package
+
+import java.lang.System;    // auto-imported by Java
+import java.lang.String;    // auto-imported by Java
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
