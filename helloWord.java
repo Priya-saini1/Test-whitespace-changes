76909411a822e42a5cd4bb7a39619eb36bf8e3d7
priya.saini <erpriyasaini2000@gmail.com>
2025-12-17 20:58:22 +0530

    whitespace

--- helloWord.java
+++ helloWord.java
@@ -1,11 +1,12 @@
-package com.example.demo;   // optional package
+  // optional package
 
 import java.lang.System;    // auto-imported by Java
 import java.lang.String;    // auto-imported by Java
 
-public class HelloWorld {
+public class helloWord {
 
     public static void main(String[] args) {
+          int a = 10;
         System.out.println("Hello, World!");
     }
 }
